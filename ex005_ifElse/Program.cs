2606ba45d4038786c1d7267a6b3c706102ea7a41
MIshka Sinichkin <mishka.jey@mail.ru>
2023-06-24 01:13:03 +0300

    Всес этой игрушкой

--- ex005_ifElse/Program.cs
+++ ex005_ifElse/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Write("Введите имя пользователя");
 string username = Console.ReadLine();
 
-if(username.ToLower() == "маша")
+if(username.ToLower() == "иконостас")
 {
-    Console.WriteLine(" Привет ,Машуня! ");
+    Console.WriteLine(" _Маша_ ");
 }
 else
 {
