4b752fc92846419bdf9183afb5306c834e5fde71
MIshka Sinichkin <mishka.jey@mail.ru>
2023-06-24 00:47:32 +0300

    Привет,Маша

---
+++ ex005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine(" Привет ,Машуня! ");
+}
+else
+{
+    Console.Write("привет");
+    Console.WriteLine(username);
+}
